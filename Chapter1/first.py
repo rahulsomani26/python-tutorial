612a348d95943978945f0fd8708b46a8b3799654
rahul-somani <somani.classes@outlook.com>
2020-11-04 18:56:23 +0530

    more meat

---
+++ Chapter1/first.py
@@ -0,0 +1,9 @@
+# import sys
+# import random
+
+# while True:
+#     print(' Type exit to exit.')
+#     response = input()
+#     if response == 'e' or response == 'E':
+#         sys.exit()
+#     print(' You typed ' + response + '.')
